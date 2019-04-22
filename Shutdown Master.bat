04607b84b71b5ffe6b08d5c8ba791f3fc218ed70
Maulem <henriquemmarti@gmail.com>
2019-04-22 16:51:47 -0300

    Update added restart function!

--- Shutdown Master.bat
+++ Shutdown Master.bat
@@ -7,12 +7,15 @@ echo.
 echo secho et /p sec=                                     
 echo :continue
 echo color 0B
+color 0c
+timeout -t 0.0001
 echo cls                                                             
 echo                              
 echo                                                                                         
 echo   %hour%   Hours
 echo   %min%  Minutes  
 echo   %sec%  Seconds
+color 0b
 echo set /a sec="%sec%-1"
 echo if %sec%==-1 set /a min="%min%-1"
 echo if %sec%==-1 set /a sec="59"
@@ -29,6 +32,7 @@ echo taskkill %image% %taskkill% %forced%
 echo exit
 echo.
 echo.
+color 0c
 echo                  uuuuuuu
 echo              uu$$$$$$$$$$$uu
 echo           uu$$$$$$$$$$$$$$$$$uu
@@ -57,12 +61,14 @@ echo    "$$$$$"                      ""$$$$""
 echo      $$$"                         $$$$"
 timeout -t 1
 cls
+color 0a
 echo Bom dia Usuario
 :ESCOLHA	
 echo O que o senhor deseja?
 echo Desligar:  1
 echo Reiniciar: 2
 echo Cancelar:  3
+echo Digite abaixo a opcao desejada:
 set /p xyz=
 if %xyz%==Desligar goto SHUTDOWN
 if %xyz%==desligar goto SHUTDOWN
@@ -73,6 +79,9 @@ if %xyz%==cancelar goto CANCEL
 if %xyz%==CANCELAR goto CANCEL
 if %xyz%==3 goto CANCEL
 if %xyz%==2 goto RESTART
+if %xyz%==Reiniciar goto RESTART
+if %xyz%==reiniciar goto RESTART
+if %xyz%==REINICIAR goto RESTART
 echo Escolha invalida!
 goto ESCOLHA
 :SHUTDOWN
